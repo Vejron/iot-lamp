93449d1e1400ee388df460303a1bd0c8b99c3fa3
Yttergren <bjorn.yttergren@gmail.com>
2017-11-08 23:04:19 +0100

    mqtt transmit color and position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {
+  MqttMessage,
+  MqttModule,
+  MqttService
+} from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements OnInit{
+  public myOtherMessage$: Observable<MqttMessage>;
+  public message;
+  
+    constructor(private mqtt: MqttService) {
+      mqtt.observe('/vejron79/lamp').subscribe((message: MqttMessage) => {
+        this.message = message.payload.toString();
+        console.log('wtf');
+      });
+      //this.myOtherMessage$ = this.mqtt.observe('my/other/topic');
+    }
+
+    ngOnInit() {
+      this.mqtt.onConnect.subscribe((e) => console.log('onConnect', e));
+      this.mqtt.onError.subscribe((e) => console.log('onError', e));
+      this.mqtt.onClose.subscribe(() => console.log('onClose'));
+      this.mqtt.onReconnect.subscribe(() => console.log('onReconnect'));
+      this.mqtt.onMessage.subscribe((e) => console.log('onMessage', e));
+    }
+  
+    public emit(msg: any): void {
+      try {
+        this.mqtt.unsafePublish('/vejron79/lamp', JSON.stringify({msg}), {qos: 0, retain: false}); 
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+  }  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatSliderModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import {
+  MqttModule,
+  MqttService
+} from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS = {
+  connectOnCreate: true,
+  hostname: 'iot.eclipse.org',
+  port: 80,
+  path: '/ws'
+};
+
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
